776fcca2bf641f362c652694a392243985ee95be
Mari-marina <83422160+Mari-marina@users.noreply.github.com>
2021-08-15 22:33:12 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__nav');
+
+button.addEventListener('click', function () {
+  menu.classList.toggle('active');
+});
+
+//# sourceMappingURL=script.js.map
